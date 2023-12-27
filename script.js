3e5a3a914a4ca204a885b847508c62870fb542af
Makson Chakma <139885747+electricmak@users.noreply.github.com>
2023-12-27 15:12:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let isConnectedToMakson = false;
 let scriptElement;
 
 function simulateTerminalResponse(response) {
-    outputDiv.innerHTML += '<br>' + '<span style="color: red;">incognito</span> |$ ';
+    outputDiv.innerHTML += '<br>' + '<span style="color: red;">INCOGNITO</span> |$ ';
     const responseElement = document.createElement('div');
     responseElement.style.color = '#55ff55';
     responseElement.style.display = "inline-block";
@@ -26,38 +26,24 @@ function simulateTerminalResponse(response) {
             cmdInput.focus();
         }
     },
-        50);
+        30);
 }
 
 cmdInput.addEventListener('keydown', async (event) => {
+    console.log('Key pressed:', event.key);
+
     if (event.key === 'Enter') {
         if (isConnectedToMakson) {
-            simulateTerminalResponse('Cannot input commands while connected to Makson. Disconnect first.');
             cmdInput.innerHTML = '';
             return;
         }
 
         const command = cmdInput.innerText.trim().toLowerCase();
-        const commandPrompt = '<span style="color: #55ff55;" id="">com.user |</span>';
+        const commandPrompt = '<span style="color: #55ff55;" id="">YOU |</span>';
         outputDiv.innerHTML += `<div style="color: #;" id="user-input">${commandPrompt}<span>${command}</span></div>`;
 
-        if (command.startsWith('vibrate')) {
-            const durationMatch = command.match(/vibrate (\d+) to (\d+)/);
 
-            if (durationMatch) {
-                const minDuration = parseInt(durationMatch[1]);
-                const maxDuration = parseInt(durationMatch[2]);
-
-                if (!isNaN(minDuration) && !isNaN(maxDuration) && minDuration <= maxDuration) {
-                    const randomDuration = Math.floor(Math.random() * (maxDuration - minDuration + 1)) + minDuration;
-                    vibrateDevice(randomDuration);
-                } else {
-                    simulateTerminalResponse('Invalid vibration range');
-                }
-            } else {
-                simulateTerminalResponse('Invalid command format. Use "vibrate [min] to [max]"');
-            }
-        } else if (command.startsWith('print')) {
+        if (command.startsWith('print')) {
             const printMatch = command.match(/print (.+) (\d+)/);
 
             if (printMatch) {
@@ -74,7 +60,6 @@ cmdInput.addEventListener('keydown', async (event) => {
             } else {
                 simulateTerminalResponse('Invalid print command format. Use "print [text] [number]"');
             }
-
         } else if (command === 'ok' || command === 'thank you' || command === 'thank' || command === 'thnx' || command === 'thanks' || command === 'then cmd') {
             const gratitudeResponses = [
                 'You\'re welcome!',
@@ -109,20 +94,17 @@ cmdInput.addEventListener('keydown', async (event) => {
             const randomResponse = doingResponses[Math.floor(Math.random() * doingResponses.length)];
             simulateTerminalResponse(randomResponse);
         } else if (command === 'makson') {
-            const maksonInfoResponse = 'Makson Chakma ☑️ \nHello, World! My name is Makson Chakma. I\m from Arunachal Pradesh studying in Bengaluru. \n My HOBBIES: \n1 | 💻 Technology,\n2 | 💁 Tips & tricks,\n3 | 🎁 Sharing my knowledges,\n My LANGUAGES: \n1 | Chakma - mother tongue,\n2 | Hindi,\n3 | English,\n4 | Assamese,\n My PROGRAMMING-LANGUAGES:\n1 | HTML,\n2 | CSS,\n3 | JAVASCRIPT,\n My GOAL \n 1 | 18,1.4,13812, 8,938 21.9,4,59,,3,13,+ along ♂️ humble man.';
+            const maksonInfoResponse = 'Makson Chakma ☑️ \nHello, World! My name is Makson Chakma. I\m from ArunachalPradesh studying in Bengaluru. \n My HOBBIES: \n1 | 💻 Technology,\n2 | 💁 Tips & tricks,\n3 | 🎁 Sharing my knowledge,\n My LANGUAGES: \n1 | Chakma - mother tongue,\n2 | Hindi,\n3 | English,\n4 | Assamese,\n My PROGRAMMING-LANGUAGES:\n1 | HTML,\n2 | CSS,\n3 | JAVASCRIPT,\n My GOAL \n 1 | 18,1.4,13812, 8,938 21.9,4,59,,3,13,+ along ♂️ humble man.';
             simulateTerminalResponse(maksonInfoResponse);
-        } else if (command === 'gps emulator' || command === 'gps' || command === 'location hacking' || command === 'hack location') {
-            const gpsInfoResponse = 'GPS Emulator Gps Emulator is a real time location hacking app free for Android users. With this you can change your location within a second. Simple to use not complicated. Remember this app is free to use for prank purpose. It\s not illegal to use this app, but make sure you should not use for illegal activities. For more below send me message from contact form.\n use "gps tool" command to get the tool';
+        } else if (command === 'gpsemulator' || command === 'gps' || command === 'location hacking' || command === 'hack location') {
+            const gpsInfoResponse = 'GPS Emulator Gps Emulator is a real-time location hacking app free for Android users. With this, you can change your location within a second. Simple to use, not complicated. Remember, this app is free to use for prank purposes. It\'s not illegal to use this app, but make sure you should not use it for illegal activities. For more info, send me a message from the contact form.\n Use "gps tool" command to get the tool';
             simulateTerminalResponse(gpsInfoResponse);
-        } else if (command === 'gps tool') {
+        } else if (command === 'sorry') {
             const gpstoolResponse = ' $ error! server down!';
             simulateTerminalResponse(gpstoolResponse);
-        }
-        else if (command === 'netsh') {
+        } else if (command === 'netsh') {
             const netshResponse = 'Unable to perform in Android!';
             simulateTerminalResponse(netshResponse);
-
-
         } else if (command === 'dir') {
             const dirResponse = 'Directory of C:\\\n\nProgram Files\nUsers\nWindows';
             simulateTerminalResponse(dirResponse);
@@ -140,9 +122,13 @@ cmdInput.addEventListener('keydown', async (event) => {
             outputDiv.insertAdjacentHTML('beforeend', '<br>incognito | $' + crushResponse) + '<br>';
             cmdInput.focus();
         } else if (command === 'fa smiley' || command === 'smiley1.png' || command === 'crush png' || command === 'crush pic 1' || command === 'crush pic1' || command === 'crush img 1' || command === 'crush img1') {
-            const imgResponse = '<div onclick="alertUser();"><img id="img" src="KrismaChakma.png" alt="Lovely Krisma Chakma"></div>\n<span id="caption">Caption: Lovely Krisma Chakma</span><br>';
-            outputDiv.insertAdjacentHTML('beforeend', '<br>MAKSON | ' + imgResponse);
+            const imgResponse = '<div onclick="alertUser();"><img id="img" src="krismaChakma.png" alt="Lovely Krisma Chakma"></div>\n<span id="caption">Caption: Lovely Krisma Chakma</span><br>';
+            outputDiv.insertAdjacentHTML('beforeend', '<br>' + imgResponse);
             cmdInput.focus();
+        } else if (command === 'gps tool') {
+            const gpstoolResponse = 'INCOGNITO | $ <a href="https://modyolo.com/gps-emulator" target="_blank">DOWNLOAD</a>';
+            outputDiv.insertAdjacentHTML('beforeend', '<br>' + gpstoolResponse);
+
         } else if (command === 'ip' || command === 'my ip' || command === 'what is my ip' || command === 'ip address') {
             getUserIP().then(ip => {
                 const ipResponse = `Your IP Address: ${ip}`;
@@ -154,30 +140,29 @@ cmdInput.addEventListener('keydown', async (event) => {
         } else if (command === 'clear history' || command === 'clear') {
             outputDiv.innerHTML = '<div class="user-input">AI-justin26: All chat has been deleted!</div><br>';
         } else if (command === 'about you' || command === 'about yourself') {
-            const aboutmeResponse = 'About my boss:\n Name - Makson Chakma \n Mother Name - Smt Gulsoki Chakma\n Father Name - Shri Sukhadev Chakma\n DOB - 26/09/2003\n POB - Mpen, Mio, Arunachal\n BIO ~ P Makson Chakma: 🌌 A seeker of joy and wisdom, dancing through lifes highs and lows. Radiating kindness, curiosity, and resilience.';
+            const aboutmeResponse = 'About my boss:\n Name - Makson Chakma \n Mother Name - Smt Gulsoki Chakma\n Father Name - Shri Sukhadev Chakma\n DOB - 26/09/2003\n POB - Mpen, Mio, Arunachal\n BIO ~ P Makson Chakma: 🌌 A seeker of joy and wisdom, dancing through life\'s highs and lows. Radiating kindness, curiosity, and resilience.';
             simulateTerminalResponse(aboutmeResponse);
         } else if (command === 'about asha' || command === 'asha yuni' || command === 'asha' || command === 'yuni') {
-            const aboutashayuniResponse = 'About Asha Yuni:\n Name - Asha Yuni \n Mother Name - unable to fetch!\n Father Name - unable ro fetch!\n DOB - 18/04/2006\n POB - unable to fetch!\n BIO ~ Asha Yuni, a vibrant 9th-grade student, finds joy in exploring the richworld of Korean culture. Her passion for learning and embracing diversity shines through in her academic journey.';
+            const aboutashayuniResponse = 'About Asha Yuni:\n Name - Asha Yuni \n Mother Name - unable to fetch!\n Father Name - unable to fetch!\n DOB - 18/04/2006\n POB - unable to fetch!\n BIO ~ Asha Yuni, a vibrant 9th-grade student, finds joy in exploring the rich world of Korean culture. Her passion for learning and embracing diversity shines through in her academic journey.';
             simulateTerminalResponse(aboutashayuniResponse);
-        } else if (command.startsWith('countdown')) {
+        } else if (command === 'face' || command === 'show your face' || command === 'id') {
+            const facetextResponse = '********************************************\niiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii\n********************************************\n︻╦̵̵͇̿̿̿̿╤──';
+            simulateTerminalResponse(facetextResponse);
+        } else if (command === 'krisma' || command === 'krisma chakma') {
+            const krismaimgResponse = '';
+            simulateTerminalResponse(krismaimgResponse);
+        } 
+        else if (command === 'gun') {
+            const gunResponse = '1.<::::::::::::::::[]==o                                                \n2.o==[]::::::::::::::::>\n3.︻╦̵̵͇̿̿̿̿╤──';
+            simulateTerminalResponse(gunResponse);
+        }
+        else if (command.startsWith('countdown')) {
             const countdownDuration = parseInt(command.split(' ')[1]);
 
             if (!isNaN(countdownDuration) && countdownDuration > 0) {
                 initiateCountdown(countdownDuration);
             } else {
                 simulateTerminalResponse('Invalid countdown duration');
-            }
-        } else if (command === 'connect makson.js' || command === 'connect with makson' || command === 'connect to makson' || command === 'connect makson') {
-            const connectResponse = 'Connecting to makson.js...';
-            simulateTerminalResponse(connectResponse);
-
-            if (!scriptElement) {
-                scriptElement = document.createElement('script');
-                scriptElement.src = 'makson.js';
-                document.body.appendChild(scriptElement);
-                isConnectedToMakson = true;
-            } else {
-                simulateTerminalResponse('Already connected to makson.js');
             }
         } else {
             simulateTerminalResponse('Command not recognized');
@@ -233,29 +218,8 @@ const displayFullUserIPv4 = async () => {
 
 window.onload = displayFullUserIPv4;
 
-function vibrateDevice(duration) {
-    if (navigator.vibrate) {
-        const startTime = new Date().getTime();
-        const endTime = startTime + duration;
-
-        simulateTerminalResponse(`Vibrating for ${duration} milliseconds...`);
-
-        const countdownInterval = setInterval(() => {
-            const currentTime = new Date().getTime();
-
-            if (currentTime >= endTime) {
-                clearInterval(countdownInterval);
-                simulateTerminalResponse('Vibrate complete');
-                cmdInput.focus();
-            } else {
-                const remainingTime = Math.ceil((endTime - currentTime) / 1000);
-                simulateTerminalResponse(`Vibrating for ${remainingTime}s`);
-            }
-        },
-            1000);
-
-        navigator.vibrate(duration);
-    } else {
-        simulateTerminalResponse('Error: Vibration not supported on this device.');
-    }
-}
+// When file is not found, show a message
+window.addEventListener('error', function (e) {
+    const errorMessage = `Welcome to incognito terminal.\n if you dont know anything type "help"\n `;
+    simulateTerminalResponse(errorMessage);
+});
